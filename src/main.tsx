78a2ba99efc39a6914f9a07b8addc1e820c1a671
nagarajsingh <78536711+nagarajsingh@users.noreply.github.com>
2026-08-04 16:49:01 +0530

    Load deployment management styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,12 @@ import App from "./App";
 import "./styles.css";
 import "./feature.css";
 import "./notifications.css";
-import "./layout-fixes.css";
+import "./deployment-management.css";
 import "./features/monitoring/monitoring.css";
 import "./features/monitoring/categories.css";
 import "./features/monitoring/details.css";
 import "./features/monitoring/orange-premium.css";
+import "./features/monitoring/table-pagination.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
