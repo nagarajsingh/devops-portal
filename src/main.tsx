2b27fa09dccab6256de427f46a06401f71095939
nagarajsingh <78536711+nagarajsingh@users.noreply.github.com>
2026-09-11 18:02:12 +0530

    Load deployment workflow refinement styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./styles.css";
 import "./feature.css";
 import "./notifications.css";
 import "./deployment-management.css";
+import "./deployment-management-refinements.css";
 import "./features/monitoring/monitoring.css";
 import "./features/monitoring/categories.css";
 import "./features/monitoring/details.css";
